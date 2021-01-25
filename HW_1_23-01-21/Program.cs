0291507001e12b0bdf00513ec6f5814658787bb1
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-01-25 12:19:16 +0500

    Добавьте файлы проекта.

---
+++ HW_1_23-01-21/Program.cs
@@ -0,0 +1,144 @@
+﻿    using System;
+
+namespace HW_1_23_01_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Task 2
+            //Console.Write("Enter numberA = ");
+            //int numberA = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter numberB = ");
+            //int numberB = Convert.ToInt32(Console.ReadLine());
+            //int result = 0;
+            //Console.Write("Odd numbers: ");
+            //for (int i = numberA; i <= numberB; i++)
+            //{
+            //    result += i;
+            //    if (i % 2 == 1)
+            //    {
+            //        Console.Write(i+" ");
+            //    }
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine($"Result = {result}");
+
+            //Task 3
+            //Rectangle
+            //for (int i = 0; i <= 8; i++)
+            //{
+            //    if (i == 0 || i == 8)
+            //    {
+            //        for (int j = 0; j <= 17; j++)
+            //        {
+            //            Console.Write(" *");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.Write(" *");
+            //        for (int j = 0; j <= 16; j++)
+            //        { Console.Write("  "); }
+            //    }
+            //    Console.Write(" *\n");
+            //}
+
+            //Triangle
+            //Console.Write("  *\n");
+            //for (int i = 0; i <= 8; i++)
+            //{
+            //    Console.Write("  *");
+            //    for (int j = 0; j <= i; j++)
+            //        Console.Write("   ");
+            //    Console.Write("*\n");
+            //    if (i == 8)
+            //    {
+            //        for (int j = 0; j <= i; j++)
+            //        { Console.Write("  * "); }
+            //        Console.Write("\n");
+            //    }
+            //}
+
+            //for (int i = 0; i <= 12; i++)
+            //{
+            //    for (int k = 12 - i; k >= 0; k--)
+            //    {
+            //        Console.Write("  ");
+            //    }
+            //    if (i >= 2 && i < 12)
+            //    {
+            //        Console.Write("  *");
+            //        for (int l = 0; l <= i - 2; l++)
+            //        {
+            //            Console.Write("    ");
+            //        }
+            //        Console.Write("   *");
+            //    }
+            //    else
+            //    {
+            //        for (int j = 0; j <= i; j++)
+            //        { Console.Write("  * "); }
+            //    }
+            //    Console.Write("\n");
+            //}
+            //for (int i = 0; i <= 12; i++)
+            //{
+            //    for (int k = 12 - i; k >= 0; k--)
+            //    {
+            //        Console.Write("  ");
+            //    }
+            //    if (i >= 2 && i <= 12)
+            //    {
+            //        Console.Write("  *");
+            //        for (int l = 0; l <= i - 2; l++)
+            //        {
+            //            Console.Write("    ");
+            //        }
+            //        Console.Write("   *");
+            //    }
+            //    else if (i == 0 || i == 1)
+            //    {
+            //        for (int j = 0; j <= i; j++)
+            //        { Console.Write("  * "); }
+            //    }
+            //    Console.Write("\n");
+            //}
+            //for (int i = 12; i >= 0; i--)
+            //{
+            //    if (i != 12)
+            //        for (int k = -i + 12; k >= 0; k--)
+            //        {
+            //            Console.Write("  ");
+            //        }
+            //    if (i >= 2 && i < 12)
+            //    {
+            //        Console.Write("  *");
+            //        for (int l = 0; l <= i - 2; l++)
+            //        {
+            //            Console.Write("    ");
+            //        }
+            //        Console.Write("   *");
+            //    }
+            //    else if (i == 0 || i == 1)
+            //    {
+            //        for (int j = 0; j <= i; j++)
+            //        { Console.Write("  * "); }
+            //    }
+            //    if (i != 12) Console.Write("\n");
+            //}
+
+            //Task 4
+            //double Persentage, sum = 1000; int month = 0;
+            //Console.Write("P = ");
+            //Persentage = double.Parse(Console.ReadLine());
+            //while (sum <= 1100)
+            //{
+            //    sum = sum + sum * (Persentage / 100);
+            //    month++;
+            //}
+            //Console.WriteLine($"Month = {month}");
+            //Console.WriteLine($"Sum = {sum}");
+        }
+    }
+}
